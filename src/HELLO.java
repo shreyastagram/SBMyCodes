4cb7b2af2792a858f51c567c9d02968b28665a53
shreyastagram <124891362+shreyastagram@users.noreply.github.com>
2024-09-29 21:16:30 +0530

    SpiralLOT ltr

---
+++ src/HELLO.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class HELLO {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        int data = sc.nextInt();
+
+        System.out.println(data + 1);
+    }
+}
